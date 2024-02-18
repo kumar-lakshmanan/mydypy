bc85a2959851891b950e9cd88ab3be2ca9e4840b
kaymatrix <kaymatrix@gmail.com>
2024-02-18 07:03:03 +0530

    tried vue integeration

--- pyone/urls.py
+++ pyone/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.HomeView.as_view(), name='home'),
+    path('page2/', views.PageTwo, name='page2'),
     path('pyone/', views.hello, name='hello'),
 ]
--- pyone/views.py
+++ pyone/views.py
@@ -1,4 +1,28 @@
+import json
+import random
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+
+
+def PageTwo(request):    
+    names = ("bob", "dan", "jack", "lizzy", "susan")
+
+    items = []
+    for i in range(100):
+        items.append({
+            "name": random.choice(names),
+            "age": random.randint(20,80),
+            "url": "https://example.com",
+        })
+
+    context = {}
+    context["items_json"] = json.dumps(items)
+
+    return render(request, 'page2.html', context)
 
 def hello(request):
     return HttpResponse("Hello, World!")
